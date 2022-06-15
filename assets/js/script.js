638eb0aa31728263c6d0e86bc46a2325003545f9
ChucktownTiger <clemsonalum07@gmail.com>
2022-06-14 22:09:55 -0400

    code back working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
-        uri: 'spotify:user:borac124:playlist:1hHLFEx94CEANlFQQCh4KT'
-      };
-      console.log(element)
+        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+        };
+        console.log(element)
     let callback = (EmbedController) => {document.querySelectorAll('ul#episodes > li > button').forEach(
         episode => {
-          episode.addEventListener('click', () => {
-              EmbedController.loadUri(episode.dataset.spotifyId)
-          });
+            episode.addEventListener('click', () => {
+                EmbedController.loadUri(episode.dataset.spotifyId)
+            });
         })
     };
     IFrameAPI.createController(element, options, callback);
