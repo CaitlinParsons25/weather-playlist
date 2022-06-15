1a469222ae0f4ae842c7d392a148b14ac25fa02f
ChucktownTiger <clemsonalum07@gmail.com>
2022-06-14 21:36:33 -0400

    initial commit java

---
+++ assets/java/script.js
@@ -0,0 +1,18 @@
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    let element = document.getElementById('embed-iframe');
+    let options = {
+        uri: 'spotify:user:borac124:playlist:1hHLFEx94CEANlFQQCh4KT'
+      };
+      console.log(element)
+    let callback = (EmbedController) => {document.querySelectorAll('ul#episodes > li > button').forEach(
+        episode => {
+          episode.addEventListener('click', () => {
+              EmbedController.loadUri(episode.dataset.spotifyId)
+          });
+        })
+    };
+    IFrameAPI.createController(element, options, callback);
+  };
+
+    
+
